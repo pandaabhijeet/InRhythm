a58654626fca407d80256ac393dbaff2e07d88ee
Abhijeet Panda <abhijeetpanda06@gmail.com>
2024-03-14 10:32:28 +0530

    initialising the user login to spotify function

--- Initializer/src/main/java/com/inrhythm/Initializer/InitializerApplication.java
+++ Initializer/src/main/java/com/inrhythm/Initializer/InitializerApplication.java
@@ -4,8 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class InitializerApplication {
 
 	public static final Logger logger = LoggerFactory.getLogger(InitializerApplication.class);
@@ -14,5 +17,16 @@ public class InitializerApplication {
 		//SpotifyTokenAccessController accessController = new SpotifyTokenAccessController();
 
 		logger.info("I am starting now !!!");
+	}
+
+	@GetMapping("/")
+	public String home(){
+		String loginUrl = "http://localhost:8080/login";
+		return "Welcome! Login to Spotify : " + "<a href=\"" + loginUrl + "\">" + "Spotify" + "</a>";
+	}
+
+	@GetMapping("/login")
+	public String login(){
+		return "Welcome! Login to Spotify : ";
 	}
 }
